88d079f634f21f16df0c81ece5861cee5d05fcd4
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-12-08 19:34:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    student_array.each do |name|
+      self.send
+    end
+    @@all << self
   end
 
   def add_student_attributes(attributes_hash)
