cb4a89cce21ce2547d95196c8d1c25096ca437a1
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-12-08 15:39:05 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,13 @@ require 'pry'
 require 'nokogiri'
 
 doc = Nokogiri::HTML(open(index_url))
+binding.pry
 class Scraper
 
   def self.scrape_index_page(index_url)
 
   end
-  binding.pry
+  
 
   def self.scrape_profile_page(profile_url)
 
