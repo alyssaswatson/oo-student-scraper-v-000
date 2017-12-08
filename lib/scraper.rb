263a10386f6ce6f4f8a97a3c1382e43a87745931
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-12-08 19:07:55 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ class Scraper
 
 
   def self.scrape_profile_page(profile_url)
-    individual_student = []
+    individual_student = Hash.new
     doc = Nokogiri::HTML(open(profile_url))
     profile_quote = doc.css(".profile-quote").text
     bio = doc.css(".description-holder").css("p").text
