b9af670c44891a15544a666dcf8e597da4efb742
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-12-08 18:43:34 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     students = []
-    doc = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
+    doc = Nokogiri::HTML(open(index_url))
     doc.css(".student-card").each do |student|
     profile_url = student.css("a").attribute("href").value
     location = student.css(".student-location").text
@@ -22,7 +22,19 @@ class Scraper
 
 
   def self.scrape_profile_page(profile_url)
-
+    individual_student = []
+    doc = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
+    doc.css(".student-card").each do |student|
+    profile_url = student.css("a").attribute("href").value
+    location = student.css(".student-location").text
+    name = student.css(".student-name").text
+    students.push(
+        name: name,
+        location: location,
+        profile_url: profile_url,
+      )
+    end
+    students
   end
 
 end
